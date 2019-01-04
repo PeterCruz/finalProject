c73d94dd279633bdd30f0d8ea6df5cdb731c5dda
Pedro Cruz <pedro.cruz@blackinntech.com>
2019-01-03 20:14:21 -0600

    Added build in back

---
+++ back-project/public/precache-manifest.d74cbaec5c48d83fe194c28ecb2c2102.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1483894cb589819c58c0aa11190b97b",
+    "url": "/static/media/logo1.d1483894.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "759485f115cf2dc8df43",
+    "url": "/static/js/main.759485f1.chunk.js"
+  },
+  {
+    "revision": "3c4f2d24fc76623c7bdd",
+    "url": "/static/js/1.3c4f2d24.chunk.js"
+  },
+  {
+    "revision": "759485f115cf2dc8df43",
+    "url": "/static/css/main.3c933a05.chunk.css"
+  },
+  {
+    "revision": "37667eef160cf744fe132dd2faed1d53",
+    "url": "/index.html"
+  }
+];
